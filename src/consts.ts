f7ab9bc45c44460fec046c5c2bb6ec052714cd28
Samuel Horn af Rantzien <samuel@samuelhorn.com>
2024-12-30 14:06:04 +0100

    favicon and consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "samuelhorn.com";
+export const SITE_DESCRIPTION =
+  "I’m sharing what works for me as a developer and creator — tools, tips, and processes that help me build meaningful projects.";
