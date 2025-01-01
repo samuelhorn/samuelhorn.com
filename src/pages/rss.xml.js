0f1640517ae069f9086d23210958e277c028a280
Samuel Horn af Rantzien <samuel@samuelhorn.com>
2025-01-01 16:19:45 +0100

    fixed xml feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
-      link: `/posts/${post.data.properties.slug}/`,
+      link: `/posts/${post.data.id}/`,
     })),
   });
 }
